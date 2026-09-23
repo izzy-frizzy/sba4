8cfd53300b4b2ffe6a9081584ab6ccf6b6cc3fdf
isaias <isaias.villa123456@gmail.com>
2026-09-23 17:00:52 -0400

    added readme file

--- script.js
+++ script.js
@@ -8,18 +8,32 @@ let taskStatus = document.getElementById("status");
 let addTaskButton = document.getElementById("addTaskButton");
 let taskL = document.getElementById("taskList");
 
+let filterInput = document.getElementById("filterInput");
+let filterButton = document.getElementById("filterButton")
+
 addTaskButton.addEventListener("click", function () {
+  
   let tasks = {
     task: taskLabel.value,
     category: categoryInput.value,
     deadLine: DeadLineStatus.value,
     status: taskStatus.value,
   };
+  if(tasks.task === "" || tasks.category === "" || tasks.deadLine === "" || tasks.status === ""){
+    alert("Please enter in each input field!");
+    return;
+
+  }
 
   taskList.push(tasks);
   showTasks();
   checkOverdueTasks();
   
+  taskLabel.value = "";
+  categoryInput.value = "";
+  DeadLineStatus.value = "";
+  taskStatus.value = "";
+
   function checkOverdueTasks() {
   let today = new Date();
   today.setHours(0, 0, 0, 0);
@@ -36,6 +50,21 @@ addTaskButton.addEventListener("click", function () {
   
 });
 
+filterButton.addEventListener("click", function(){
+let filter = filterInput.value
+
+filterItem(filter)
+function filterItem(searchTerm) {
+  let terms = [];
+  for (i = 0; i < taskList.length; i++) {
+    if (taskList[i] === searchTerm) {
+      terms.push(taskList[i]);
+    }
+  }
+  return terms;
+}
+});
+
 function showTasks() {
   taskL.innerHTML = "";
 
@@ -46,14 +75,4 @@ function showTasks() {
 
     taskL.appendChild(taskItem);
   }
-}
-
-function filterItem(searchTerm) {
-  let terms = [];
-  for (i = 0; i < shoppingCart.length; i++) {
-    if (shoppingCart[i].includes(searchTerm)) {
-      terms.push(shoppingCart[i]);
-    }
-  }
-  return terms;
 }
